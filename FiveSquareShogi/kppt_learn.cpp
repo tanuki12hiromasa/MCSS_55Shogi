7b17860d5d21fe579b1958391d738403926c2730
Iwamoto Hiromasa <tanuki13.snow@gmail.com>
2020-05-20 17:22:57 +0900

    特徴量ベクトルを生成する関数

---
+++ FiveSquareShogi/kppt_learn.cpp
@@ -0,0 +1,84 @@
+﻿#include "kppt_learn.h"
+
+namespace kppt {
+	kppt_paramVector::kppt_paramVector(const SearchPlayer& player) {
+		KPP = new KPPEvalVectorFloat1[SquareNum];
+		KKP = new KKPEvalVectorFloat1[SquareNum];
+		set(player);
+	}
+
+	void kppt_paramVector::set(const SearchPlayer& player) {
+		for (int h = 0; h < SquareNum; h++) {
+			for (int i = 0; i < fe_end; i++) {
+				for (int j = 0; j < fe_end; j++) {
+					KPP[h][i][j] = { 0,0 };
+				}
+			}
+			for (int i = 0; i < SquareNum; i++) {
+				for (int j = 0; j < fe_end; j++) {
+					KKP[h][i][j] = { 0,0 };
+				}
+			}
+		}
+		const unsigned skpos = player.kyokumen.sOuPos();
+		const unsigned gkpos = player.kyokumen.gOuPos();
+		auto* ppskpp = KPP[skpos];
+		auto* ppgkpp = KPP[inverse(gkpos)];
+		if (player.kyokumen.teban()) {
+			for (int i = 0; i < EvalList::EvalListSize; ++i) {
+				const int k0 = player.feature.idlist.list0[i];
+				const int k1 = player.feature.idlist.list1[i];
+				auto* pskpp = ppskpp[k0];
+				auto* pgkpp = ppgkpp[k1];
+				for (int j = 0; j < EvalList::EvalListSize; j++) {
+					if (i == j)continue;
+					const int l0 = player.feature.idlist.list0[j];
+					const int l1 = player.feature.idlist.list1[j];
+					pskpp[l0][0] += 1; pskpp[l0][1] += 1;
+					pgkpp[l1][0] -= 1; pgkpp[l1][1] += 1;
+				}
+				KKP[skpos][gkpos][k0][0] += 1;
+				KKP[gkpos][skpos][k1][0] -= 1;
+				KKP[skpos][gkpos][k0][1] += 1;
+				KKP[gkpos][skpos][k1][1] += 1;
+			}
+		}
+		else {
+			for (int i = 0; i < EvalList::EvalListSize; ++i) {
+				const int k0 = player.feature.idlist.list0[i];
+				const int k1 = player.feature.idlist.list1[i];
+				auto* pskpp = ppskpp[k0];
+				auto* pgkpp = ppgkpp[k1];
+				for (int j = 0; j < EvalList::EvalListSize; j++) {
+					if (i == j)continue;
+					const int l0 = player.feature.idlist.list0[j];
+					const int l1 = player.feature.idlist.list1[j];
+					pskpp[l0][0] -= 1; pskpp[l0][1] += 1;
+					pgkpp[l1][0] += 1; pgkpp[l1][1] += 1;
+				}
+				KKP[skpos][gkpos][k0][0] -= 1;
+				KKP[gkpos][skpos][k1][0] += 1;
+				KKP[skpos][gkpos][k0][1] += 1;
+				KKP[gkpos][skpos][k1][1] += 1;
+			}
+		}
+	}
+
+	kppt_paramVector::kppt_paramVector(kppt_paramVector&& rhs) noexcept {
+		KPP = rhs.KPP;
+		KKP = rhs.KKP;
+		rhs.KPP = nullptr;
+		rhs.KKP = nullptr;
+	}
+
+	kppt_paramVector& kppt_paramVector::operator=(kppt_paramVector&& rhs)noexcept {
+		delete[] KPP;
+		delete[] KKP;
+		KPP = rhs.KPP;
+		KKP = rhs.KKP;
+		rhs.KPP = nullptr;
+		rhs.KKP = nullptr;
+		return *this;
+	}
+
+}
---
+++ FiveSquareShogi/kppt_learn.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include "kppt_evaluate.h"
+
+namespace kppt {
+	using EvalVectorFloat = std::array<float, 2>;
+	using KPPEvalVectorFloat0 = EvalVectorFloat[fe_end];
+	using KPPEvalVectorFloat1 = KPPEvalVectorFloat0[fe_end];
+	using KPPEvalVectorFloat2 = KPPEvalVectorFloat1[SquareNum];
+	using KKPEvalVectorFloat0 = EvalVectorFloat[fe_end];
+	using KKPEvalVectorFloat1 = KKPEvalVectorFloat0[SquareNum];
+	using KKPEvalVectorFloat2 = KKPEvalVectorFloat1[SquareNum];
+
+	class kppt_paramVector {
+	public:
+		kppt_paramVector(const SearchPlayer&);
+		kppt_paramVector(kppt_paramVector&&)noexcept;
+		kppt_paramVector& operator=(kppt_paramVector&&)noexcept;
+		kppt_paramVector(const kppt_paramVector&) = delete;
+		kppt_evaluator& operator=(const kppt_paramVector&) = delete;
+
+		void set(const SearchPlayer&);
+	private:
+		KPPEvalVectorFloat1* KPP;
+		KKPEvalVectorFloat1* KKP;
+
+	public:
+		kppt_paramVector& operator+=(const kppt_paramVector& rhs);
+		kppt_paramVector& operator-=(const kppt_paramVector& rhs);
+		kppt_paramVector& operator*=(const double c);
+
+		friend class kppt_learn;
+		friend class ShogiTest;
+	};
+
+	class kppt_learn {
+		static void updateEvalParam(const kppt_paramVector&);
+	};
+}
